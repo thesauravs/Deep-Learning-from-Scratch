acf6e102df46312ea02497d763aa12742fc912da
Saurav Shah <thesaurav.sh@gmail.com>
2019-10-20 10:16:48 +0530

    Update randn VS rand

--- week1/randnVSrand.py
+++ week1/randnVSrand.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sample_size = 100000
+sample_size = 1000000
 uniform = np.random.rand(sample_size)
+print(uniform)
 normal = np.random.randn(sample_size)
+print(normal)
 
 pdf, bins, patches = plt.hist(uniform, bins=50, range=(0, 1), density=True)
 plt.title('rand: uniform')
