f8f75f553550966124e9e2ee8f2c02c5470cdc63
Saurav Shah <thesaurav.sh@gmail.com>
2020-02-13 00:45:27 +0530

    MNIST Fashion

---
+++ mnist_fashion_practice.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/ 255
+test_images = test_images/ 255
+
+#print(train_images[1])
+#
+#plt.imshow(train_images[1], cmap = plt.cm.binary)
+#plt.show
+model = keras.Sequential([
+        keras.layers.Flatten(input_shape = (28,28)),
+        keras.layers.Dense(128, activation = 'relu'),
+        keras.layers.Dense(10, activation = 'softmax')
+        ])
+
+model.compile(optimizer = 'adam', loss = "sparse_categorical_crossentropy",
+              metrics = ['accuracy'])
+
+model.fit(train_images, train_labels, epochs = 10)
+
+pred = model.predict(test_images)
+print(class_names[np.argmax(pred[0])])
